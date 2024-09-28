7e907d994067632678241f66cb40cc7e9b113998
TiernanJesrani <134875734+TiernanJesrani@users.noreply.github.com>
2024-09-28 13:21:08 -0400

    base class

--- leetcode/leetcodeClass.py
+++ leetcode/leetcodeClass.py
@@ -5,3 +5,13 @@ class Leetcode:
     def get_progress_weekly():
         return (1, 5)
     
+    def get_problem_list():
+        test_object = ("Name", "https://leetcode.com/problems/design-circular-deque/description/?envType=daily-question&envId=2024-09-28", 
+         "Difficulty")
+        return [test_object]
+    
+    def get_problems_completed():
+        return []
+    
+
+    
